9a8991618efaf0d1a4ad42f6f96c1da84979dd17
Bisheshops <bisheshshr@gmail.com>
2026-04-27 11:48:36 +0545

    Window Render Complete

--- main.cpp
+++ main.cpp
@@ -6,6 +6,45 @@ int main(int argc, char *argv[])
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         std::cerr << "Failed to initialize SDL:" << SDL_GetError() << "\n";
+        return 1;
     }
+
+    SDL_Window *window = SDL_CreateWindow("Narrative Roguelike - Engine Test.", 800, 600, 0);
+    if (!window)
+    {
+        std::cerr << "Failed to create window:" << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer)
+    {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    bool running = true;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 20, 20, 25, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
