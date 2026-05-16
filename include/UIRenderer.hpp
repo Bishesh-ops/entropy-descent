7165c426f23f22ca0326a2ff6513165ac9ab5292
Bisheshops <bisheshshr@gmail.com>
2026-05-16 10:35:51 +0545

    Pivot: Clear C++ Source fo Odin Port

--- include/UIRenderer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SDL3/SDL.h>
-#include <entt/entt.hpp>
-
-class UIRenderer
-{
-public:
-    void render(SDL_Renderer *renderer, const entt::registry &registry, entt::entity playerEntity, int windowWidth, int windowHeight, int floorDepth);
-};
